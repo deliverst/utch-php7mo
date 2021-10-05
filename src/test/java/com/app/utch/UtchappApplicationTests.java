45a410f58a0b83a9406df24cc8b9a66e82ee42de
Javier Valles <deliverst@icloud.com>
2021-10-04 19:00:27 -0600

    add spring proyect

---
+++ src/test/java/com/app/utch/UtchappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.utch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtchappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
