37bf5b4022918532a98751327ad22a0fc1f0694d
Javier Valles <deliverst@icloud.com>
2021-10-06 15:26:20 -0600

    creation news enitys

---
+++ src/main/java/com/app/utch/persistence/entity/ComprasProducto.java
@@ -0,0 +1,10 @@
+package com.app.utch.persistence.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "compras_producto")
+public class ComprasProducto {
+
+}
---
+++ src/main/java/com/app/utch/persistence/entity/Product.java
@@ -0,0 +1,87 @@
+package com.app.utch.persistence.entity;
+
+import javax.persistence.*;
+import javax.swing.text.StyledEditorKit;
+
+@Entity //esto se comporta como una clase que mapea una tabla de la base de datos define el resto de la arquitectura de nuestro modelo
+@Table(name = "productos")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //
+    @Column(name = "id_producto") //esto se pone por que cambio
+    private Integer idProducto; //es una mala practica poner _ al declarar las variables y por eso usamos camelcase, y en la parte de arriba hacemos referencia @Col para decir la columna original como esta en la base de datos
+
+    private String nombre;
+
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name = "codigo_barras")
+    private String codigoBarras;
+
+    @Column(name = "precio_venta")
+    private Double precioVenta;
+
+    @Column(name = "cantidad_stock")
+    private Integer cantidadStock;
+
+    private Boolean estado;
+
+//    Getters and Setters
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public Double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Integer getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(Integer cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
